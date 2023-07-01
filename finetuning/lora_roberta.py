c86ffba1fd4ba862a1cb83da345131624704152f
Arijit Sehanobish <arijitthegame@gmail.com>
2023-07-01 16:58:44 -0400

    working roberta code

--- finetuning/lora_roberta.py
+++ finetuning/lora_roberta.py
@@ -15,17 +15,18 @@ from peft import (
     PeftType,
     PrefixTuningConfig,
     PromptEncoderConfig,
+    PeftModelForSequenceClassification
 ) 
 
 #TODO : Adapter Tuning
 
 import evaluate
 from datasets import load_dataset
-from transformers import AutoModelForSequenceClassification, AutoTokenizer, get_linear_schedule_with_warmup, set_seed
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, get_linear_schedule_with_warmup, PretrainedConfig
 from tqdm import tqdm
 import wandb
 
-logger = get_logger(__name__)
+logger = logging.getLogger(__name__)
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
@@ -46,7 +47,7 @@ peft_types_map = {'lora' : PeftType.LORA,
                 'p_tuning' : PeftType.P_TUNING,
                 'prefix_tuning': PeftType.PREFIX_TUNING,
                 'adalora' : PeftType.ADALORA,
-    'adaptation_prompt': PeftType.ADAPTION_PROMPT
+                'adaptation_prompt': PeftType.ADAPTION_PROMPT
     }
 
 wandb.init(project='peft-glue-finetuning')
@@ -57,8 +58,8 @@ parser = argparse.ArgumentParser(
 parser.add_argument("--seed", default=42, type=int)
 parser.add_argument("--batch_size", default=32, type=int)
 parser.add_argument("--model_name_or_path", default="roberta-large", type=str) # can use roberta-base or other bert models
-parser.add_argument("--task", default='mrpc', type=str)
-parser.add_argument("--peft_type", default='lora', choices=FUNCTION_MAP.keys())
+parser.add_argument("--task_name", default='mrpc', type=str)
+parser.add_argument("--peft_type", default='lora', choices=peft_types_map.keys())
 parser.add_argument("--device", default='cuda')
 parser.add_argument("--num_epochs", default=20, type=int)
 parser.add_argument("--lr", default=3e-4, type=float)
@@ -75,20 +76,14 @@ parser.add_argument(
 parser.add_argument("--weight_decay", type=float, default=0.0, help="Weight decay to use.")
 
 
-
-
 args = parser.parse_args()
 np.random.seed(args.seed)
 random.seed(args.seed)
+torch.manual_seed(args.seed)
 
-config = AutoConfig.from_pretrained(args.model_name_or_path, num_labels=num_labels, finetuning_task=args.task_name)
-tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path, padding_side='right')
-model = AutoModelForSequenceClassification.from_pretrained(args.model_name_or_path, config=config)
+peft_type = args.peft_type
 
-if getattr(tokenizer, "pad_token_id") is None:
-    tokenizer.pad_token_id = tokenizer.eos_token_id
-
-raw_datasets = load_dataset("glue", args.task)
+raw_datasets = load_dataset("glue", args.task_name)
 
 if args.task_name is not None:
     is_regression = args.task_name == "stsb"
@@ -109,52 +104,61 @@ else:
         label_list.sort()  # Let's sort it for determinism
         num_labels = len(label_list)
 
+
+config = AutoConfig.from_pretrained(args.model_name_or_path, num_labels=num_labels, finetuning_task=args.task_name)
+tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path, padding_side='right')
+model = AutoModelForSequenceClassification.from_pretrained(args.model_name_or_path, config=config, return_dict=True)
+
+if getattr(tokenizer, "pad_token_id") is None:
+    tokenizer.pad_token_id = tokenizer.eos_token_id
+
  # Preprocessing the datasets
-    if args.task_name is not None:
-        sentence1_key, sentence2_key = task_to_keys[args.task_name]
-    else:
+if args.task_name is not None:
+    sentence1_key, sentence2_key = task_to_keys[args.task_name]
+else:
         # Again, we try to have some nice defaults but don't hesitate to tweak to your use case.
-        non_label_column_names = [name for name in raw_datasets["train"].column_names if name != "label"]
-        if "sentence1" in non_label_column_names and "sentence2" in non_label_column_names:
-            sentence1_key, sentence2_key = "sentence1", "sentence2"
+    non_label_column_names = [name for name in raw_datasets["train"].column_names if name != "label"]
+    if "sentence1" in non_label_column_names and "sentence2" in non_label_column_names:
+        sentence1_key, sentence2_key = "sentence1", "sentence2"
+    else:
+        if len(non_label_column_names) >= 2:
+            sentence1_key, sentence2_key = non_label_column_names[:2]
         else:
-            if len(non_label_column_names) >= 2:
-                sentence1_key, sentence2_key = non_label_column_names[:2]
-            else:
-                sentence1_key, sentence2_key = non_label_column_names[0], None
+            sentence1_key, sentence2_key = non_label_column_names[0], None
 
     # Some models have set the order of the labels to use, so let's make sure we do use it.
-    label_to_id = None
-    if (
-        model.config.label2id != PretrainedConfig(num_labels=num_labels).label2id
-        and args.task_name is not None
-        and not is_regression
-    ):
+label_to_id = None
+if (
+    model.config.label2id != PretrainedConfig(num_labels=num_labels).label2id
+    and args.task_name is not None
+    and not is_regression
+):
         # Some have all caps in their config, some don't.
-        label_name_to_id = {k.lower(): v for k, v in model.config.label2id.items()}
-        if sorted(label_name_to_id.keys()) == sorted(label_list):
-            logger.info(
+    label_name_to_id = {k.lower(): v for k, v in model.config.label2id.items()}
+    if sorted(label_name_to_id.keys()) == sorted(label_list):
+        logger.info(
                 f"The configuration of the model provided the following label correspondence: {label_name_to_id}. "
                 "Using it!"
             )
-            label_to_id = {i: label_name_to_id[label_list[i]] for i in range(num_labels)}
-        else:
-            logger.warning(
+        label_to_id = {i: label_name_to_id[label_list[i]] for i in range(num_labels)}
+    else:
+        logger.warning(
                 "Your model seems to have been trained with labels, but they don't match the dataset: ",
                 f"model labels: {sorted(label_name_to_id.keys())}, dataset labels: {sorted(label_list)}."
                 "\nIgnoring the model labels as a result.",
             )
-    elif args.task_name is None and not is_regression:
-        label_to_id = {v: i for i, v in enumerate(label_list)}
+elif args.task_name is None and not is_regression:
+    label_to_id = {v: i for i, v in enumerate(label_list)}
 
-    if label_to_id is not None:
-        model.config.label2id = label_to_id
-        model.config.id2label = {id: label for label, id in config.label2id.items()}
-    elif args.task_name is not None and not is_regression:
-        model.config.label2id = {l: i for i, l in enumerate(label_list)}
-        model.config.id2label = {id: label for label, id in config.label2id.items()}
+if label_to_id is not None:
+    model.config.label2id = label_to_id
+    model.config.id2label = {id: label for label, id in config.label2id.items()}
+elif args.task_name is not None and not is_regression:
+    model.config.label2id = {l: i for i, l in enumerate(label_list)}
+    model.config.id2label = {id: label for label, id in config.label2id.items()}
 
 padding = "max_length" if args.pad_to_max_length else False
+
 def preprocess_function(examples):
         # Tokenize the texts
     texts = (
@@ -217,10 +221,10 @@ optimizer_grouped_parameters = [
             "weight_decay": 0.0,
         },
     ]
-optimizer = AdamW(optimizer_grouped_parameters, lr=args.learning_rate)
+optimizer = AdamW(optimizer_grouped_parameters, lr=args.lr, weight_decay=args.weight_decay)
 
 with open('lora_config_glue.json', 'r') as f:
-    peft_config = json.loads(f)
+    peft_config = json.load(f)
 
 peft_config = LORAConfig(peft_config)
 
@@ -270,10 +274,11 @@ for epoch in range(num_epochs):
     model.save_pretrained(peft_model_id)
     wandb.log({'train_loss' : loss_epoch})
     wandb.log({'val_acc': eval_metric})
-    d[epoch] = eval_accuracy
+    d[epoch] = eval_metric
 
 
-# load best model based on validation accuracy 
+# load best model based on validation accuracy, this ckpt line will throw an error
+# TODO: Fix this dict issue 
 ckpt = max(d, key=d.get)
 peft_model_path = f"{args.model_name_or_path}_{args.peft_type}_{ckpt}"
 inference_model = AutoModelForSequenceClassification.from_pretrained(args.model_name_or_path, config=config)
