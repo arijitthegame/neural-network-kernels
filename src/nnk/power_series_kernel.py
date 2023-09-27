395ff367d7b8e32c89cd3b7562df2ddc4390e8a3
Arijit Sehanobish <arijitthegame@gmail.com>
2023-09-26 21:57:27 -0400

    lint

--- src/nnk/power_series_kernel.py
+++ src/nnk/power_series_kernel.py
@@ -10,14 +10,15 @@ from scipy.stats import rv_discrete
 
 # breaking up the power series of gelu into positive and negative
 
-def generate_rademacher_samples(shape, complex_weights=False, device='cpu'):
-    """ Draws uniformly from the (complex) Rademacher distribution. """
+
+def generate_rademacher_samples(shape, complex_weights=False, device="cpu"):
+    """Draws uniformly from the (complex) Rademacher distribution."""
     if complex_weights:
         support = torch.tensor([1j, -1j, 1, -1], dtype=torch.complex64, device=device)
     else:
         support = torch.tensor([1, -1], dtype=torch.float32, device=device)
-    #samples = torch.index_select(support, 0, torch.randint(len(support), shape).view(-1))
-    #return samples.reshape(shape)
+    # samples = torch.index_select(support, 0, torch.randint(len(support), shape).view(-1))
+    # return samples.reshape(shape)
     indices = torch.randint(len(support), shape)
     return support[indices]
 
@@ -38,7 +39,7 @@ class P_Measure(rv_discrete):
         h01: Whether to set p(0)=p(1)=0
         """
         if p <= 1:
-            raise RuntimeError('p needs to be greater than 1!')
+            raise RuntimeError("p needs to be greater than 1!")
 
         self.p = p
         self.h01 = h01
@@ -48,88 +49,102 @@ class P_Measure(rv_discrete):
 
     def _pmf(self, k):
         if self.max_val == np.inf:
-            norm_const = 1./(self.p-1.)
+            norm_const = 1.0 / (self.p - 1.0)
         else:
-            norm_const = np.sum(np.array(
-                [1./self.p**(n+1.) for n in range(self.max_val+1)]
-            ))
-        pmf_vals = 1./(self.p**(k+1.))
+            norm_const = np.sum(
+                np.array([1.0 / self.p ** (n + 1.0) for n in range(self.max_val + 1)])
+            )
+        pmf_vals = 1.0 / (self.p ** (k + 1.0))
         pmf_vals[k > self.max_val] = 0
 
         # there is no point in wasting features on the constant
-        pmf_vals[k==0] = 0
-        norm_const = norm_const - 1./self.p
+        pmf_vals[k == 0] = 0
+        norm_const = norm_const - 1.0 / self.p
 
         if self.h01:
-            norm_const = norm_const - 1./(self.p**2)
-            pmf_vals[k==1] = 0
+            norm_const = norm_const - 1.0 / (self.p ** 2)
+            pmf_vals[k == 1] = 0
 
         return pmf_vals / norm_const
-    
-
-def create_random_feats(inputs, num_rfs, degree, coef_fun_pos, coef_fun_neg,  p=2, M=1, seed=0):
-  
- 
-  torch.manual_seed(seed)
-  np.random.seed(seed)
-  random.seed(seed)
-  measure = P_Measure(p, True, degree)
-
-  feats = num_rfs - 1
-
-  degrees = measure.rvs(size=feats)
-  # print(degrees)
-        # degrees are sorted from highest to lowest
-  degrees, proj_dims = np.unique(np.array(degrees), return_counts=True)
-
-  coefs_pos = [coef_fun_pos(x) for x in degrees]
-  coefs_neg = [coef_fun_neg(x) for x in degrees]
-
-  # ensures unbiasedness of maclaurin estimator
-  coefs_pos /= measure._pmf(degrees)
-  coefs_neg /= measure._pmf(degrees)
-
-  modules = []
-  for degree, dim in zip(degrees, proj_dims):
-      # we skip the constant
-      # the bias and lengthscales will already be included in the data
-      # FIX
-      omega = generate_rademacher_samples((dim, degree, inputs.shape[-1]))
-      modules.append(omega)
-
-  # possible rescaling required
-  features_pos = []
-  features_neg = []
-  for i in range(len(modules)):
-      if len(inputs.shape) == 2 :
-        a = torch.prod(torch.einsum('ij, bkj-> bik' ,inputs, modules[i]), dim=-1)
-        features_pos.append(a.t() * np.sqrt(coefs_pos[i])*p) #do we need this p
-        features_neg.append(a.t() * np.sqrt(coefs_neg[i])*p)
-      elif len(inputs.shape) == 3:
-        a = torch.prod(torch.einsum('bij, dkj-> dbik ', inputs, modules[i]), dim=-1)
-        features_pos.append(torch.permute(a, (1,2,0))* np.sqrt(coefs_pos[i]) * p )
-        features_neg.append(torch.permute(a, (1,2,0))* np.sqrt(coefs_neg[i]) * p )
 
 
-  features_pos = torch.cat(features_pos, dim=-1)
-  features_pos = features_pos / np.sqrt(num_rfs) #should it be rfs or rfs-1
-  
-  features_neg = torch.cat(features_neg, dim=-1)
-  features_neg = features_neg / np.sqrt(num_rfs) #should it be rfs or rfs-1
+def create_random_feats(
+    inputs, num_rfs, degree, coef_fun_pos, coef_fun_neg, p=2, M=1, seed=0
+):
 
-  if len(inputs.shape)== 2:
-    add_features_pos = torch.Tensor([coef_fun_pos(0)]).float().sqrt().repeat(len(inputs), 1)
-    all_features_pos = torch.cat([add_features_pos, features_pos], dim=1)
-    add_features_neg = torch.Tensor([coef_fun_neg(0)]).float().sqrt().repeat(len(inputs), 1)
-    all_features_neg = torch.cat([add_features_neg, features_neg], dim=1)
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    measure = P_Measure(p, True, degree)
 
-  elif len(inputs.shape) == 3:
-    add_features_pos = torch.Tensor([coef_fun_pos((0))]).float().sqrt().expand((features.shape[0],features.shape[1], 1))
-    all_features_pos = torch.cat([add_features_pos, features_pos], dim=-1)
-    add_features_neg = torch.Tensor([coef_fun_neg((0))]).float().sqrt().expand((features.shape[0],features.shape[1], 1))
-    all_features_neg = torch.cat([add_features_neg, features_neg], dim=-1)
+    feats = num_rfs - 1
 
-  else :
-    raise ValueError('Unsupported tensor shape')
+    degrees = measure.rvs(size=feats)
+    # print(degrees)
+    # degrees are sorted from highest to lowest
+    degrees, proj_dims = np.unique(np.array(degrees), return_counts=True)
 
-  return torch.cat([features_pos, features_neg], dim=-1)
+    coefs_pos = [coef_fun_pos(x) for x in degrees]
+    coefs_neg = [coef_fun_neg(x) for x in degrees]
+
+    # ensures unbiasedness of maclaurin estimator
+    coefs_pos /= measure._pmf(degrees)
+    coefs_neg /= measure._pmf(degrees)
+
+    modules = []
+    for degree, dim in zip(degrees, proj_dims):
+        # we skip the constant
+        # the bias and lengthscales will already be included in the data
+        # FIX
+        omega = generate_rademacher_samples((dim, degree, inputs.shape[-1]))
+        modules.append(omega)
+
+    # possible rescaling required
+    features_pos = []
+    features_neg = []
+    for i in range(len(modules)):
+        if len(inputs.shape) == 2:
+            a = torch.prod(torch.einsum("ij, bkj-> bik", inputs, modules[i]), dim=-1)
+            features_pos.append(a.t() * np.sqrt(coefs_pos[i]) * p)  # do we need this p
+            features_neg.append(a.t() * np.sqrt(coefs_neg[i]) * p)
+        elif len(inputs.shape) == 3:
+            a = torch.prod(torch.einsum("bij, dkj-> dbik ", inputs, modules[i]), dim=-1)
+            features_pos.append(torch.permute(a, (1, 2, 0)) * np.sqrt(coefs_pos[i]) * p)
+            features_neg.append(torch.permute(a, (1, 2, 0)) * np.sqrt(coefs_neg[i]) * p)
+
+    features_pos = torch.cat(features_pos, dim=-1)
+    features_pos = features_pos / np.sqrt(num_rfs)  # should it be rfs or rfs-1
+
+    features_neg = torch.cat(features_neg, dim=-1)
+    features_neg = features_neg / np.sqrt(num_rfs)  # should it be rfs or rfs-1
+
+    if len(inputs.shape) == 2:
+        add_features_pos = (
+            torch.Tensor([coef_fun_pos(0)]).float().sqrt().repeat(len(inputs), 1)
+        )
+        all_features_pos = torch.cat([add_features_pos, features_pos], dim=1)
+        add_features_neg = (
+            torch.Tensor([coef_fun_neg(0)]).float().sqrt().repeat(len(inputs), 1)
+        )
+        all_features_neg = torch.cat([add_features_neg, features_neg], dim=1)
+
+    elif len(inputs.shape) == 3:
+        add_features_pos = (
+            torch.Tensor([coef_fun_pos((0))])
+            .float()
+            .sqrt()
+            .expand((features.shape[0], features.shape[1], 1))
+        )
+        all_features_pos = torch.cat([add_features_pos, features_pos], dim=-1)
+        add_features_neg = (
+            torch.Tensor([coef_fun_neg((0))])
+            .float()
+            .sqrt()
+            .expand((features.shape[0], features.shape[1], 1))
+        )
+        all_features_neg = torch.cat([add_features_neg, features_neg], dim=-1)
+
+    else:
+        raise ValueError("Unsupported tensor shape")
+
+    return torch.cat([features_pos, features_neg], dim=-1)
--- src/nnk/taylor_series.py
+++ src/nnk/taylor_series.py
@@ -3,6 +3,7 @@ import numpy as np
 
 ####### Get tanh coefficients and split them into positive series and a negative series
 
+
 def get_tanh_coeffs(n, cache):
     if n == 0:
         return 1
@@ -17,44 +18,59 @@ def get_tanh_coeffs(n, cache):
     cache[n] = result
     return result
 
+
 def get_positive_tanh_coeffs(n):
-  if n%2 == 0:
-    return 0
-  elif n%4 == 1:
-    return get_tanh_coeffs(int(n-1/2), {})
-  else :
-    return 0
+    if n % 2 == 0:
+        return 0
+    elif n % 4 == 1:
+        return get_tanh_coeffs(int(n - 1 / 2), {})
+    else:
+        return 0
+
 
 def get_negative_tanh_coeffs(n):
-  if n%2 == 0:
-    return 0
-  elif n%4 == 3:
-    return get_tanh_coeffs(int(n-1/2), {})
-  else :
-    return 0
-  
-######## Get gelu coefficients and split into positive and negative series 
+    if n % 2 == 0:
+        return 0
+    elif n % 4 == 3:
+        return get_tanh_coeffs(int(n - 1 / 2), {})
+    else:
+        return 0
+
+
+######## Get gelu coefficients and split into positive and negative series
+
 
 def pos_gelu_coeffs(n):
-  if n == 1 :
-    return 1/math.sqrt(math.pi)
-  if n == 0:
-    return 0
-  if (n>1) and (n%2 == 1):
-    return 0
-  if (n>1) and (n%4 == 0):
-    return 1/math.sqrt(math.pi) * 1/ (math.sqrt(2)**(n-1) * (n-1) * np.math.factorial(int(n/2) -1))
-  if n%4 == 2:
-    return 0
+    if n == 1:
+        return 1 / math.sqrt(math.pi)
+    if n == 0:
+        return 0
+    if (n > 1) and (n % 2 == 1):
+        return 0
+    if (n > 1) and (n % 4 == 0):
+        return (
+            1
+            / math.sqrt(math.pi)
+            * 1
+            / (math.sqrt(2) ** (n - 1) * (n - 1) * np.math.factorial(int(n / 2) - 1))
+        )
+    if n % 4 == 2:
+        return 0
+
 
 def neg_gelu_coeffs(n):
-  if n == 0:
-    return 0
-  if n == 1:
-    return 0
-  if n % 2 == 1:
-    return 0
-  if n % 4 == 2 :
-    return 1/math.sqrt(math.pi) * 1/ (math.sqrt(2)**(n-1) * (n-1) * np.math.factorial(int(n/2) -1))
-  if n%4 == 0:
-    return 0
+    if n == 0:
+        return 0
+    if n == 1:
+        return 0
+    if n % 2 == 1:
+        return 0
+    if n % 4 == 2:
+        return (
+            1
+            / math.sqrt(math.pi)
+            * 1
+            / (math.sqrt(2) ** (n - 1) * (n - 1) * np.math.factorial(int(n / 2) - 1))
+        )
+    if n % 4 == 0:
+        return 0
