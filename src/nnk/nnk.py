233dcdf0e743269cec643817d7c3b6489af52cf1
Arijit Sehanobish <arijitthegame@gmail.com>
2023-09-26 21:56:51 -0400

    add v1 code

--- src/nnk/nnk.py
+++ src/nnk/nnk.py
@@ -2,6 +2,8 @@ import torch
 import math
 from torch import nn
 
+eps = 1e-10
+
 from performer_attention import gaussian_orthogonal_random_matrix
 
 
@@ -84,6 +86,85 @@ Args :
         raise ValueError("Unsupported tensor shape of xw")
     diff_vector = dot_products - correction_vector
     return (1.0 / math.sqrt(num_rfs)) * AB_coeffs * torch.exp(diff_vector)
+
+
+
+def torch_apply_along_axis_asym_kernel(function, x, axis, b_term, device):
+    """
+    v1 version of the torch_apply_along_axis 
+    b_term is the bias so always should be a vector.
+    """
+    if b_term is None:
+      batched_f = torch.vmap(function, in_dims=(0))
+      return batched_f(x)
+    else:
+      if len(b_term.shape) == 1:
+        batched_f = torch.vmap(function, in_dims=(0, 0))
+        xis_broadcasted = torch.add(torch.zeros(len(b_term),1).to(device), x)
+        return batched_f(xis_broadcasted, b_term.reshape((-1,1)))
+      else :
+        raise ValueError("Unsuported Tensor shape")
+
+def create_asym_feats(xw, AB_fun, ab_fun, xis, num_rfs, dim, device,
+                                  seed, normalize, normalization_constant,
+                                  orthogonal, proj_matrix, bias_term, M, is_weight):
+    """
+    V1 version of the above and code for equation 10 in the paper, 
+    Note this creates asymmetric features as different features are created if the input is W or X. 
+    """
+    if normalize :
+      if normalization_constant is None :
+        xw = torch.nn.functional.normalize(xw)
+        bias_term = bias_term - bias_term.min()/ (bias_term.max() - bias_term.min()) + eps
+      else :
+        xw = normalization_constant * torch.nn.functional.normalize(xw)
+        bias_term = normalization_constant *  bias_term - bias_term.min()/ (bias_term.max() - bias_term.min()+0.000001) # this will set bias_term to be None if input bias are zeros
+    ab_coeffs = torch_apply_along_axis_asym_kernel(ab_fun, xis, 0, None, device)
+    AB_coeffs = torch_apply_along_axis_asym_kernel(AB_fun, xis, 0, bias_term, device) # needs to be a vector. each entry correspond to a bias term
+    # if bias is None, then we can just compute AB_coeffs once
+    torch.manual_seed(seed)
+    ab_coeffs = ab_coeffs.squeeze()
+    ab_coeffs = ab_coeffs.to(device)
+    AB_coeffs = AB_coeffs.to(device)
+
+    if proj_matrix is None:
+      if device == 'cpu':
+        if orthogonal is False :
+          gs = torch.normal(mean=0, std=1, size=(num_rfs, dim))
+        else :
+          gs = gaussian_orthogonal_random_matrix(num_rfs, dim, scaling = 0, device = 'cpu')
+      else :
+        if orthogonal is False :
+          gs = torch.normal(mean=0, std=1, size=(num_rfs, dim)).to(device)
+        else :
+          gs = gaussian_orthogonal_random_matrix(num_rfs, dim, scaling = 0, device = 'cuda')
+    else :
+      if device == 'cpu':
+        gs = proj_matrix
+      else :
+         gs = proj_matrix.cuda()
+    renorm_gs = (np.sqrt(1+4.0 * M) * ab_coeffs * gs.t()).t()
+    if len(xw.shape) == 2 :
+      # renorm_gs has complex numbers, and torch.eimsum needs both inputs to be complex doubles
+      dot_products = torch.einsum('ij,jk->ik', xw.type(torch.cfloat), renorm_gs.t()) #complex128 / cfloat
+    elif len(xw.shape) == 3:
+      dot_products = torch.einsum('bij,jk->bik', xw.type(torch.cfloat), renorm_gs.t())
+    else :
+      raise ValueError("Unsuported Tensor shape")
+    squared_xw = torch.sum(torch.mul(xw, xw), dim=-1) #do not keepdims here
+    if len(squared_xw.shape) == 1 :
+      correction_vector = torch.outer(squared_xw / 2, torch.mul(ab_coeffs, ab_coeffs))
+    elif len(squared_xw.shape) == 2 :
+      correction_vector = torch.einsum('pq, r->pqr', squared_xw / 2, torch.mul(ab_coeffs, ab_coeffs))
+    else :
+      raise ValueError("Unsupported tensor shape of xw")
+    correction_vector += M * torch.linalg.norm(gs, axis=-1) * torch.linalg.norm(gs, axis=-1)
+    # check the shape of dot_products and correction vector
+    diff_vector = dot_products - correction_vector
+    if is_weight:
+      return (1.0 / math.sqrt(num_rfs)) * AB_coeffs * torch.exp(diff_vector)
+    else: # is x
+      return (1.0 / math.sqrt(num_rfs)) * torch.exp(diff_vector)
 
 
 def phi_relu_mapping_torch(
@@ -274,3 +355,58 @@ class NNK_Relu(nn.Module):
             proj_matrix=self.projection_matrix,
         )
         return output_x @ self.weights.t()
+
+
+class NNK_Asym_Kernel(nn.Module) :
+  def __init__(self, input_weights, A_fun, a_fun, xis, num_rfs, dim, model_device, seed, normalize, B_fun, b_fun, M, b):
+        super().__init__()
+        self.input_weights = input_weights
+        self.A_fun = A_fun
+        self.a_fun = a_fun
+        self.xis = xis
+        self.num_rfs = num_rfs
+        self.dim = dim
+        self.model_device = model_device
+        self.seed = seed
+        self.normalize = normalize
+        self.B_fun = B_fun
+        self.b_fun = b_fun
+        self.M = M
+        self.b = b
+
+        # hack using the BERT initialization here
+        self.projection_matrix = torch.normal(mean=0, std=.02, size=(self.num_rfs, self.dim)).to(self.model_device)
+
+        if self.normalize is False:
+          self.input_weights = self.input_weights/self.dim**.25 #.5
+          self.b = self.b/self.dim**.25 #.5
+
+        self.first_rfv_plus = create_asym_feats(xw=self.input_weights, AB_fun=self.B_fun, ab_fun=self.b_fun, xis=self.xis, num_rfs=self.num_rfs
+                                                        , dim=self.dim, device=self.model_device, seed=self.seed, normalize=self.normalize,
+                                                   normalization_constant=None, orthogonal=False, proj_matrix=self.projection_matrix, bias_term = self.b, M=self.M,
+                                                            is_weight=True)
+        self.first_rfv_minus = create_asym_feats(xw=self.input_weights, AB_fun=self.B_fun, ab_fun=self.b_fun, xis=-self.xis, num_rfs=self.num_rfs
+                                                        , dim=self.dim, device=self.model_device, seed=self.seed, normalize=self.normalize,
+                                                   normalization_constant=None, orthogonal=False, proj_matrix=self.projection_matrix, bias_term = self.b, M=self.M,
+                                                             is_weight=True)
+        self.weights = (1.0 / np.sqrt(2.0)) * np.power(1.0 + 4.0 * self.M, self.dim / 4.0) * torch.cat(
+            [self.first_rfv_plus, self.first_rfv_minus], dim=-1)
+
+        self.weights = nn.Parameter(self.weights)
+
+  def forward(self, x):
+
+        if self.normalize is False:
+          x = x/self.dim
+
+        x_rfv_plus = create_asym_feats(xw=x, AB_fun=self.A_fun, ab_fun=self.a_fun, xis=self.xis, num_rfs=self.num_rfs
+                                                        , dim=self.dim, device=self.model_device, seed=self.seed, normalize=self.normalize,
+                                                   normalization_constant=None, orthogonal=False, proj_matrix=self.projection_matrix, bias_term = self.b, M=self.M,
+                                                   is_weight=False)
+        x_rfv_minus = create_asym_feats(xw=x, AB_fun=self.A_fun, ab_fun=self.a_fun, xis=-self.xis, num_rfs=self.num_rfs
+                                                        , dim=self.dim, device=self.model_device, seed=self.seed, normalize=self.normalize,
+                                                   normalization_constant=None, orthogonal=False, proj_matrix=self.projection_matrix, bias_term = self.b, M=self.M,
+                                                    is_weight=False)
+        output_x = (1.0 / np.sqrt(2.0)) * np.power(1.0 + 4.0 * self.M, self.dim / 4.0) * torch.cat([x_rfv_plus, x_rfv_minus], dim=-1)
+
+        return (output_x @ self.weights.t()).real
